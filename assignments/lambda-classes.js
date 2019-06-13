cf6440ee3b5cb0efe226b5443816192a78a0c735
Joscelyn <JoscelynRose@gmail.com>
2019-06-13 16:25:30 -0400

    finished MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -26,7 +26,7 @@ class Person {
         this.location = attributes.location;
     }
     speak() {
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
 
@@ -40,10 +40,10 @@ class Instructor extends Person {
         this.catchPhrase = instructorAttributes.catchPhrase;
     }
     demo(subject) {
-        console.log(`Today we are learning about ${subject}`);
+        return `Today we are learning about ${subject}`;
     }
     grade(student, subject) {
-        console.log(`${student.name} receives a perfect score on ${subject}`);
+        return `${student.name} receives a perfect score on ${subject}`;
     }
 }
 
@@ -56,13 +56,13 @@ class Student extends Person {
         this.favSubjects = studentAttributes.favSubjects;
     }
     listsSubjects() {
-        console.log(`this.favSubjects`);
+        return `${this.favSubjects}`;
     }
     PRAssignment(subject) {
-        console.log(`${this.name} has submitted a PR for ${subject}`);
+        return `${this.name} has submitted a PR for ${subject}`;
     }
     sprintChallenge(subject) {
-        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+        return `${this.name} has begun sprint challenge on ${subject}`;
     }
 }
 
@@ -73,10 +73,150 @@ class ProjectManager extends Instructor {
         this.favInstructor = projectManagerAttributes.favInstructor;
     }
     standUp(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`);
+        return `${this.name} announces to ${channel}, @channel standy times!​​​​​`;
     }
     debugsCode(student, subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
     }
 
 }
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 'Infinity',
+    location: 'Denver',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!',
+});
+
+const matthew = new Instructor({
+    name: 'Matthew',
+    age: 29,
+    location: 'Toronto',
+    specialty: 'eating donuts',
+    favLanguage: 'French',
+    catchPhrase: 'ne pas subir',
+});
+
+  const isaiah = new Student({
+    name: 'Isaiah',
+    age: 18,
+    location: 'Florida',
+    previousBackground: 'High School last month',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+    const kevin = new Student({
+    name: "Kevin",
+    age: 28,
+    location: "California",
+    previousBackground: "Table Games Dealer",
+    className: "WEB21",
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+    const nisa = new Student({
+    name: 'Nisa',
+    age: 25,
+    location: 'Ohio',
+    previousBackground: 'Debt Collector',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+
+    const joscelyn = new Student({
+    name: "Joscelyn",
+    age: 29,
+    location: "California",
+    previousBackground: "English teacher",
+    className: 'Web21',
+    favSubjects: ["Computer Science", "Philosophy", "English"],
+});
+
+  const marguel = new ProjectManager({
+    name: 'Marguel',
+    age: 'Maybe 26',
+    gradClassName: 'WEBPT2',
+    favInstructor: 'Me?',
+    location: 'California',
+    specialty: 'React',
+    favLanguage: 'JavaScript, Python, Elm etc.',
+    catchPhrase: "Practice Flex Zombies !!!",
+});
+
+  const brandon = new ProjectManager({
+    name: 'Brandon',
+    age: '34',
+    gradClassName: 'WEB18',
+    favInstructor: 'Professor Lambda',
+    location: 'Maine',
+    specialty: 'Redux',
+    favLanguage: 'JavaScript, C++, Python.',
+    catchPhrase: "You shall not pass!",
+});
+
+    const mary = new ProjectManager({
+    name: 'Mary',
+    age: '24',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: "That looks AWESOME",
+});
+
+    const christian = new ProjectManager({
+    name: 'Christian',
+    age: '32',
+    gradClassName: 'WEB18',
+    favInstructor: 'Every Instructor in Lambda',
+    location: 'Seattle, WA',
+    specialty: 'Data Structures & Algorithms',
+    favLanguage: 'JavaScript',
+    catchPhrase: "Dont forget your daily commit.",
+});
+
+    const pat = new ProjectManager({
+    name: 'Pat',
+    age: '38',
+    gradClassName: 'WEB18',
+    favInstructor: 'Brett Madrid',
+    location: 'Petaluma, Ca',
+    specialty: 'Empathetic to the struggle of Redux',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Lets google that together.'
+});
+
+    const darren = new ProjectManager({
+    name: 'Darren',
+    age: '25',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'North Carolina',
+    specialty: 'React',
+    favLanguage: 'Javascript',
+    catchPhrase: 'Gang. Gang.',
+});
+
+    const austin = new ProjectManager({
+    name: 'Austin',
+    age: '23',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'Somewhere',
+    specialty: 'Java',
+    favLanguage: 'Java',
+    catchPhrase: ":eggplant:",
+});
+
+
+console.log(austin.name);// Austin
+console.log(matthew.location);// Toronto
+console.log(kevin.age);// 28
+console.log(dan.speak());// Hello my name is Dan, I am from Denver
+console.log(joscelyn.listsSubjects())// Computer Science,Philosophy,English
+console.log(isaiah.sprintChallenge('prototypical inheritance'));// Isaiah has begun sprint challenge on prototypical inheritance
+console.log(pat.catchPhrase);//Lets google that together
+console.log(mary.debugsCode(joscelyn, 'CSS'));// Mary debugs Joscelyn's code on CSS
+console.log(dan.grade(joscelyn, 'JavaScript'));//Joscelyn receives a perfect score on JavaScript
