01d748db016c9c64e435a17c1a7ccd2be9b76fcc
Joscelyn <JoscelynRose@gmail.com>
2019-06-13 16:02:32 -0400

    finished student constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -58,23 +58,14 @@ class Student extends Person {
     listsSubjects() {
         console.log(`this.favSubjects`);
     }
-    PRAssignment(student, subject) {
-        console.log(`${student.name} receives a perfect score on ${subject}`);
+    PRAssignment(subject) {
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    }
+    sprintChallenge(subject) {
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
     }
 }
 
-// #### Student
-
-// * Now we need some students!
-// * Student uses the same attributes that have been set up by Person
-// * Student has the following unique props:
-//   * `previousBackground` i.e. what the Student used to do before Lambda School
-//   * `className` i.e. CS132
-//   * `favSubjects`. i.e. an array of the student's favorite subjects ['Html', 'CSS', 'JavaScript']
-// * Student has the following methods:
-//   * `listsSubjects` a method that logs out all of the student's favoriteSubjects one by one.
-//   * `PRAssignment` a method that receives a subject as an argument and logs out that the `student.name has submitted a PR for {subject}`
-//   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 // #### Project Manager
 
